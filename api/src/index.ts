91db710ab061b25486db0d92a6c45d54bcf96a41
Mila Sager <ms3421@ic.ac.uk>
2023-05-29 16:53:48 +0100

    Added express, start:dev scripts and set both static serve and api serve paths

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world!");
+import express from "express";
+
+const app = express();
+
+app.get("/api", (_, res) => {
+  res.writeHead(200);
+  res.write("Hello World! I am the API.");
+  res.end();
+})
+
+app.use(express.static("../public/drp-37/dist"));
+
+app.listen(80, () => {
+  console.log("Server is now listening for connections on port 80! :3");
+})
