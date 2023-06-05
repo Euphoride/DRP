826db3e35a23e6c3233b16a1cd34f803ca75526e
Mila Sager <ms3421@ic.ac.uk>
2023-06-05 15:55:19 +0100

    Front-end cleanup of excess CSS and refactoring notifications code

--- public/drp-37/src/App.tsx
+++ public/drp-37/src/App.tsx
@@ -1,93 +1,9 @@
-
 import { Component, createResource } from 'solid-js';
+import { PushedNotificationRecord, arePushNotificationsSupported, getPermission, getRemindMes, postRemindMe, registerServiceWorker, sendSubscription, subscribeToNotifications } from './notifications/Notification';
 
 import styles from './App.module.css';
 
-type PushedNotificationRecord = {
-  id: number,
-  date_posted: Date
-};
 
-async function getRemindMes(): Promise<PushedNotificationRecord[]> {
-  const response = await fetch("/api/remindme");
-  const raw_data = await response.json();
-
-  // This guy here is meant to be validating the type of the data we just fetched.
-  // Does this a couple of ways:
-  //   1. The option chaining on message defaults to null rather than an exception if
-  //      message doesn't exist. || [] means that if it is null, the default value it
-  //      should take is the empty list.
-  //   2. We flatmap because its easy to return an "empty" (None in Maybe) case of 
-  //      "this record didn't fit the type but i want to check the next ones". 
-  // Thank God for monads.
-  
-  const data: PushedNotificationRecord[] = raw_data.message?.flatMap((item: any) => {
-    if (!item.id || !item.date_posted) return [];
-
-    return [{id: item.id as number, date_posted: new Date(item.date_posted as string)}];
-  }) || [];
-
-  return data;
-}
-
-async function postRemindMe(timestamp: Date): Promise<void> {
-  await fetch("/api/remindme", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({timestamp: timestamp.getTime() / 1000})
-  });
-}
-
-function arePushNotificationsSupported(): boolean {
-  return "serviceWorker" in navigator && "PushManager" in window;
-}
-
-function getPermission(): Promise<NotificationPermission> {
-  return Notification.requestPermission(x => x);
-}
-
-// function sendRemindMeNotification() {
-//   const image = "/assets/71ggBUmny9L.jpg";
-//   const text = "This is your reminder!";
-//   const title = "Remind Me";
-
-//   const options = {
-//     body: text,
-//     icon: image,
-//     vibrate: [200, 100, 200],
-//     tag: "remind-me",
-//     image: image,
-//     badge: image,
-//     actions:  [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
-//   };
-
-//   navigator.serviceWorker.ready.then((serviceWorker) => {
-//     serviceWorker.showNotification(title, options);
-//   })
-// }
-
-function registerServiceWorker() {
-  navigator.serviceWorker.register("/sw.js");
-}
-
-function subscribeToNotifications(pushServerPublicKey: string): Promise<PushSubscription> {
-  return navigator.serviceWorker.ready.then(async (sw) => {
-    const subscription = await sw.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: pushServerPublicKey
-    });
-
-    return subscription;
-  })
-}
-
-async function sendSubscription(subscription: PushSubscription, customText: string) {
-  await fetch("/api/subscribe", {
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({subscription: subscription, text: customText}),
-    method: "POST"
-  });
-}
 
 const RemindMe: Component<PushedNotificationRecord> = (props) => {
   const string_date = props.date_posted.toString();
---
+++ public/drp-37/src/notifications/Notification.tsx
@@ -0,0 +1,67 @@
+export function arePushNotificationsSupported(): boolean {
+  return "serviceWorker" in navigator && "PushManager" in window;
+}
+
+export function getPermission(): Promise<NotificationPermission> {
+  return Notification.requestPermission(x => x);
+}
+
+
+export function registerServiceWorker() {
+  navigator.serviceWorker.register("/sw.js");
+}
+
+export type PushedNotificationRecord = {
+  id: number,
+  date_posted: Date
+};
+
+export async function getRemindMes(): Promise<PushedNotificationRecord[]> {
+  const response = await fetch("/api/remindme");
+  const raw_data = await response.json();
+
+  // This guy here is meant to be validating the type of the data we just fetched.
+  // Does this a couple of ways:
+  //   1. The option chaining on message defaults to null rather than an exception if
+  //      message doesn't exist. || [] means that if it is null, the default value it
+  //      should take is the empty list.
+  //   2. We flatmap because its easy to return an "empty" (None in Maybe) case of 
+  //      "this record didn't fit the type but i want to check the next ones". 
+  // Thank God for monads.
+  
+  const data: PushedNotificationRecord[] = raw_data.message?.flatMap((item: any) => {
+    if (!item.id || !item.date_posted) return [];
+
+    return [{id: item.id as number, date_posted: new Date(item.date_posted as string)}];
+  }) || [];
+
+  return data;
+}
+
+export async function postRemindMe(timestamp: Date): Promise<void> {
+  await fetch("/api/remindme", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({timestamp: timestamp.getTime() / 1000})
+  });
+}
+
+
+export async function subscribeToNotifications(pushServerPublicKey: string): Promise<PushSubscription> {
+  return navigator.serviceWorker.ready.then(async (sw) => {
+    const subscription = await sw.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: pushServerPublicKey
+    });
+
+    return subscription;
+  })
+}
+
+export async function sendSubscription(subscription: PushSubscription, customText: string) {
+  await fetch("/api/subscribe", {
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({subscription: subscription, text: customText}),
+    method: "POST"
+  });  
+}
