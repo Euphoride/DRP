c78a98197fada54b98ca849be99c9204eb6fc2c5
Katherine Zheng <dz1721@ic.ac.uk>
2023-06-15 14:53:15 +0100

    Styled Landing page to be nicer

--- public/drp-37/src/navigation/Navigation.tsx
+++ public/drp-37/src/navigation/Navigation.tsx
@@ -2,28 +2,29 @@ import { A } from "@solidjs/router";
 import { Component } from "solid-js";
 
 import styles from "./Navigation.module.css";
+import logo from "../assets/Logo word.png";
 
 const NavPage: Component = () => {
   return (
     <div class={styles.Navigation}>
-      <h1 style="color:blue;">Closer</h1>
+      <img style="width: 70vw; padding-top: 20vh" src={logo} />
 
       <h4> I am: </h4>
-      <A href="/alex">
+      <A href="/alex" style="text-decoration: none">
         {" "}
-        <button>Alex</button>{" "}
+        <button class={styles.accent_button}>Alex</button>{" "}
       </A>
-      <A href="/betty">
+      <A href="/betty" style="text-decoration: none">
         {" "}
-        <button>Betty</button>{" "}
+        <button class={styles.accent_button}>Betty</button>{" "}
       </A>
-      <A href="/carl">
+      <A href="/carl" style="text-decoration: none">
         {" "}
-        <button> Carl</button>{" "}
+        <button class={styles.accent_button}> Carl</button>{" "}
       </A>
-      <A href="/derek">
+      <A href="/derek" style="text-decoration: none">
         {" "}
-        <button>Derek</button>{" "}
+        <button class={styles.accent_button}>Derek</button>{" "}
       </A>
     </div>
   );
